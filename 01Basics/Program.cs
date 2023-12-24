fb3491c36bbdb75915937b74d90d2dd580af538a
caueoliveiraaa <leo.siscobra@gmail.com>
2023-12-24 14:27:30 -0300

    update

---
+++ 01Basics/Program.cs
@@ -0,0 +1,20 @@
+﻿// Display information
+Console.Write("Hello, ");
+Console.Write("world");
+
+// Break line
+Console.Write("\n");
+
+// Display information and break line
+Console.WriteLine("Hello, World!");
+
+/*
+ * this 
+ * is 
+ * a
+ * multiline
+ * comment
+ */
+
+// Waits for key to be pressed to end the program
+Console.ReadKey();
---
+++ 02Variables/Program.cs
@@ -0,0 +1,14 @@
+﻿int x; // Declaration
+x = 123; // Initialization
+int y = 123; // Declaration + Initialization
+int z = x + y; // Result of sum
+int age = 21; // Whole integer
+double height = 300.5;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+Console.WriteLine("Age: " + age);
+Console.WriteLine("Height: " + height + "cm");
+
+Console.ReadKey();
