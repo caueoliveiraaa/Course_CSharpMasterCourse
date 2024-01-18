84c72d7a49c33a73403631a45ccd98fe85555f49
caueoliveiraaa <leo.siscobra@gmail.com>
2024-01-17 21:51:25 -0300

    (Cauê Oliveira - 01/17/2024)
    Studying: Learning about variables in C#.

--- TodoList/TodoList/Program.cs
+++ TodoList/TodoList/Program.cs
@@ -10,7 +10,10 @@ namespace TodoList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("[S]ee all TODOs");
+            Console.WriteLine("[A]dd a TODO");
+            Console.WriteLine("[R]emove a TODO");
+            Console.WriteLine("[E]xit");
             Console.ReadKey();
         }
     }
