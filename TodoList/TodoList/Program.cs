15e9a0731a7dd8db12d2f58fa14f1f18ed85be5d
caueoliveiraaa <leo.siscobra@gmail.com>
2024-01-20 08:55:32 -0300

    C# Fundamentals
    Learning about variables and operaors.

--- TodoList/TodoList/Program.cs
+++ TodoList/TodoList/Program.cs
@@ -14,6 +14,10 @@ namespace TodoList
             Console.WriteLine("[A]dd a TODO");
             Console.WriteLine("[R]emove a TODO");
             Console.WriteLine("[E]xit");
+
+            string userChoice = Console.ReadLine();
+            Console.WriteLine("User input " + userChoice);
+
             Console.ReadKey();
         }
     }
