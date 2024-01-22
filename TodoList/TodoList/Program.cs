c0549b6dc0c73721dd0a05a84e1d11ee00e93dcf
caueoliveiraaa <leo.siscobra@gmail.com>
2024-01-22 20:54:23 -0300

    Methods
    Learning about methods

--- TodoList/TodoList/Program.cs
+++ TodoList/TodoList/Program.cs
@@ -17,6 +17,41 @@ namespace TodoList
 
             string userChoice = Console.ReadLine();
             Console.WriteLine("User input " + userChoice);
+            Console.WriteLine($"Input {userChoice} is long: {IsLong(userChoice)}");
+            Console.WriteLine($"10 + {userChoice.Length} = {Add(10, userChoice.Length)}");
+
+
+            if (userChoice == "S")
+            {
+                PrintSelectedOption("see all TODOs");
+            }
+            else if (userChoice == "A")
+            {
+                PrintSelectedOption("add a TODO");
+            }
+            else if (userChoice == "R")
+            {
+                PrintSelectedOption("remove a TODO");
+            }
+            else if (userChoice == "E")
+            {
+                PrintSelectedOption("exit");
+            }
+
+            int Add(int a, int b)
+            {
+                return a + b;
+            }
+
+            bool IsLong(string input)
+            {
+                return input.Length > 10;
+            }
+
+            void PrintSelectedOption(string option)
+            {
+                Console.WriteLine("Selected option " + option);
+            }
 
             Console.ReadKey();
         }
