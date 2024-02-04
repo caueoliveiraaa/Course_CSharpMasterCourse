ac43a61215e550319a8352ee2de569d183a83559
caueoliveiraaa <leo.siscobra@gmail.com>
2024-02-04 18:58:01 -0300

    update

---
+++ ToDoList/ToDoList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing todolist");
+            Console.ReadKey();
+        }
+    }
+}
