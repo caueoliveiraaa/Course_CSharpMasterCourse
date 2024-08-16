c7393590a2d51579c23a5baecc02d7bdcb8ebc48
caueoliveiraaa <leo.siscobra@gmail.com>
2024-08-16 20:57:35 -0300

    fix: Naming folders
    This commit fixes the name of the folder where the main logic is located.

--- ToDoList/ToDoList/Services/TaskScreen.cs
+++ ToDoList/ToDoList/Services/TaskScreen.cs
@@ -3,7 +3,7 @@ namespace Course_CSharpMasterCourse.ToDoList.ToDoList;
 public static class TaskScreen {
 
     public static void ReadTask() {
-
+        string name = Console.ReadLine();
     }
 
     public static void ReadNewTask() {
